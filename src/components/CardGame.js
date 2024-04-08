3fb8f5690c4e61563182ed5e178ebdbfd5b202f1
Diep Tran <dnntran15@gmail.com>
2024-04-07 20:22:08 -0700

    add restart and end dialog

--- src/components/CardGame.js
+++ src/components/CardGame.js
@@ -1,7 +1,17 @@
 "use client";
 import { useState, useEffect } from 'react'
 import Card from './Card'
+import { Button } from './ui/button'
 
+  import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogClose,
+    DialogFooter,
+  } from "@/components/ui/dialog"
 
 function initList(){
     var arr = [];
@@ -16,6 +26,7 @@ function Game(){
     const [cardObjs, setCardObjs] = useState([]);
     const [prevCard, setPrevCard] = useState(-1);
     const [isValidating, setIsValidating] = useState(false);
+    const [matchedCnt, setMatchedCnt] = useState(0);
 
     useEffect(() => {
         const shuffleCards = initList();
@@ -30,6 +41,7 @@ function Game(){
         else if(cardObjs[currentIndex].id == cardObjs[prevCard].id){
             cardObjs[prevCard].status = "matched"
             cardObjs[currentIndex].status = "matched flippedC"
+            setMatchedCnt(matchedCnt+1);
             setCardObjs([...cardObjs])
             setPrevCard(-1)
         }else{
@@ -59,9 +71,32 @@ function Game(){
             validateMatch(id);
         }
     }
+    
+    function handleRestartClick(id){
+        const shuffleCards = initList();
+        setCardObjs(shuffleCards);
+        setMatchedCnt(0);
+    }
+;
+    const dialogDesc = "You've matched all the photos perfectly. You are safe from Charlie's wrath... for now."
 
     return (
     <div className="grid grid-cols-4 grid-flow-row gap-5">
+         <Dialog open={matchedCnt >= 8}>
+            <DialogContent handleClose={() => setMatchedCnt(0)}>
+            <DialogHeader>
+                <DialogTitle className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl header-center">You did it!!!</DialogTitle>
+                <DialogDescription className="smallScreen:text-lg text-xl font-bold tracking-wide welcome-desc">
+                {dialogDesc}
+                </DialogDescription>
+            </DialogHeader>
+                <DialogFooter className="justify-center sm:justify-center">
+                    <Button className="py-9 px-9 btn-grad" onClick={() => handleRestartClick()}>
+                        <p className='text-lg font-semibold	tracking-widest'>Play again!</p>
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
          {cardObjs.map(( cardObj, index ) => {
             return (
           <Card key={index} cardObj={cardObj} id={index} handleClick={handleClick} />
